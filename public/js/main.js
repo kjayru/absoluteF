4a10d9a636712970a41251e51ab1d1fbddc83c4b
Wile <wiltinoco@gmail.com>
2018-11-03 18:01:31 -0500

    evento

--- public/js/main.js
+++ public/js/main.js
@@ -309,10 +309,12 @@ function onPlayerReady() {
 
   //modal
  let mod = document.querySelector(".modal-trigger");
+ if(mod){
  mod.addEventListener('click',function(e){
      e.preventDefault();
      onPlayerReady();
  });
+}
   var _createClass = function () {function defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}return function (Constructor, protoProps, staticProps) {if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;};}();function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}var RippleModal = function () {
     function RippleModal() {var _this = this;var triggerClass = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'modal-trigger';var modalClass = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'modal';_classCallCheck(this, RippleModal);
       this.triggerClass = triggerClass;
