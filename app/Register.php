fa5969af19f852fff3867236307a1b0c5f6c95d8
Wile <wiltinoco@gmail.com>
2018-11-04 20:22:19 -0500

    modelo

--- app/Register.php
+++ app/Register.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Register extends Model
 {
     protected $fillable = [
-        'nombres','email','sucripcion','terminos'
+        'nombres','email','suscripcion','terminos'
     ];
 }
