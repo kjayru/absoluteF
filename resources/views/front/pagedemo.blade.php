9b19efaed517cea3a5a96a225d774812419554b8
kjayru <wiltinoco@gmail.com>
2018-11-20 07:50:19 -0500

    pagedemo

--- resources/views/front/pagedemo.blade.php
+++ resources/views/front/pagedemo.blade.php
@@ -8,44 +8,6 @@
   <title>demo</title>
 
 <style>
-.grid {
-  max-width: 1200px;
-}
-
-/* reveal grid after images loaded */
-.grid.are-images-unloaded {
-  opacity: 0;
-}
-
-.grid__item,
-.grid__col-sizer {
-  width: 32%;
-}
-
-.grid__gutter-sizer { width: 2%; }
-
-/* hide by default */
-.grid.are-images-unloaded .image-grid__item {
-  opacity: 0;
-}
-
-.grid__item {
-  margin-bottom: 20px;
-  float: left;
-}
-
-.grid__item--height1 { height: 140px; background: #EA0; }
-.grid__item--height2 { height: 220px; background: #C25; }
-.grid__item--height3 { height: 300px; background: #19F; }
-
-.grid__item--width2 { width: 66%; }
-
-.grid__item img {
-  display: block;
-  max-width: 100%;
-}
-
-
 .page-load-status {
   display: none; /* hidden by default */
   padding-top: 20px;
@@ -58,8 +20,6 @@
 </head>
 
 <body translate="no" >
-
-
 
 <!-- ramdon width 2 and height 1-->
 <div class="grid are-images-unloaded">
@@ -74,16 +34,11 @@
  @endphp
  @if(fmod($k, 4) == 0)
 
-  <div class="grid__item grid__item--width2">
+  <div class="grid__item grid__item--width2 {{$k}}">
     <div class="box">
-
-
-        <div class="grid__item grid__item--width2">
                 <div class="box" style="background:url(/images/scroll-bt{{$op}}.png); background-size:cover;">
 
                 </div>
-        </div>
-
     </div>
 </div>
 @else
