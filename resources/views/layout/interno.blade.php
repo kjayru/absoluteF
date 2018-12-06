26666410e284431479519d44345c2f7fa93c5675
Wile <wiltinoco@gmail.com>
2018-12-05 21:46:21 -0500

    detalles

--- resources/views/layout/interno.blade.php
+++ resources/views/layout/interno.blade.php
@@ -17,7 +17,7 @@
   </head>
   <body>
     @yield('content')
-
+    @include('partial.footer')
     <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <
