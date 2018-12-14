fdadd1dc807161a5166aeaa8db2623452139c3f6
kjayru <wiltinoco@gmail.com>
2018-12-13 19:10:41 -0500

    absoluto

--- resources/views/partial/navigationInterno.blade.php
+++ resources/views/partial/navigationInterno.blade.php
@@ -9,25 +9,18 @@
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
         <a class="nav-link btnConoce btn-effect" href="/secciones/conoce-la-botella" ><i class="fas fa-angle-left">
-            </i> Conoce la <strong>botella</strong>
-            <span></span>
+            </i> Conoce la <strong>botella</strong> <span></span>
          </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link btnInicio btn-effect" href="/secciones" >Inicio
-                <span></span>
-        </a>
-
+        <a class="nav-link btnInicio btn-effect" href="/secciones" >Inicio<span></span> </a>
       </li>
 
     </ul>
     <a class="navbar-brand" href="/secciones"><img src="/images/logo-absolut.svg" class="img-fluid"/></a>
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
-        <a class="nav-link btnVolver btn-effect" href="/secciones">
-            Volver
-        <span></span>
-    </a>
+         <a class="nav-link btnVolver btn-effect" href="/secciones"> Volver <span></span> </a>
       </li>
     </ul>
   </div>
